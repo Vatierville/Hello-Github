7f3cf4edb9fcc5fd0ae68d373e82773e7a75e818
David <waterfield.david@gmail.com>
2015-07-28 23:07:15 +0100

    bcrypt

--- BCryptTest/Program.cs
+++ BCryptTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using TicketmasterSystems.Encryption;
 
 namespace BCryptTest
 {
@@ -10,6 +12,27 @@ namespace BCryptTest
     {
         static void Main(string[] args)
         {
+            const int iterations = 1000;
+            var passwords = new List<string>();
+
+            for (int i = 0 ; i < iterations ; ++i)
+            {
+                passwords.Add("zhadum66T!" + i);
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0 ; i < iterations ; ++i)
+            {
+                string hashedPassword = BCrypt.HashPassword(passwords[i], BCrypt.GenerateSalt());
+                Console.WriteLine("{0}\t{1}", passwords[i], hashedPassword);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed = {0}", stopwatch.Elapsed);
+            Console.ReadLine();
         }
     }
 }
