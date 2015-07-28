f727275c2d992dbd0a481b08ad584e82b50901dd
David <waterfield.david@gmail.com>
2015-07-28 23:46:39 +0100

    le fin

--- BCryptTest/Program.cs
+++ BCryptTest/Program.cs
@@ -12,26 +12,31 @@ namespace BCryptTest
     {
         static void Main(string[] args)
         {
-            const int iterations = 1000;
+            const int iterations = 100;
             var passwords = new List<string>();
 
             for (int i = 0 ; i < iterations ; ++i)
             {
-                passwords.Add("zhadum66T!" + i);
+                passwords.Add("passWORD!" + i);
             }
 
             var stopwatch = new Stopwatch();
+            Console.WriteLine("Starting...");
             stopwatch.Start();
 
             for (int i = 0 ; i < iterations ; ++i)
             {
-                string hashedPassword = BCrypt.HashPassword(passwords[i], BCrypt.GenerateSalt());
-                Console.WriteLine("{0}\t{1}", passwords[i], hashedPassword);
+                string hashedPassword = BCrypt.HashPassword(passwords[i], BCrypt.GenerateSalt(4));
+                if (i < 1)
+                {
+                    Console.WriteLine("{0}\t{1}", passwords[i], hashedPassword);
+                }
             }
 
             stopwatch.Stop();
 
-            Console.WriteLine("Elapsed = {0}", stopwatch.Elapsed);
+            Console.WriteLine("Elapsed = {0}ms", stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Time per Encrypt = {0}ms", stopwatch.ElapsedMilliseconds / iterations);
             Console.ReadLine();
         }
     }
