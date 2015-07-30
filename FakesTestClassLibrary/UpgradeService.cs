a5a557d02260a0852bb1b43b6f143ac49cf6658b
David <waterfield.david@gmail.com>
2015-07-30 23:37:49 +0100

    fakes stuff

---
+++ FakesTestClassLibrary/UpgradeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakesTestClassLibrary
+{
+    public class UpgradeService : IUpgradeService
+    {
+
+        public int CurrentSWVersion(int x)
+        {
+            return 1;
+        }
+
+        public bool IsSWUpgradeRequired(int DeviceID)
+        {
+            return false;
+        }
+
+        public DateTime LastUpgradeDate(int DeviceID)
+        {
+            return DateTime.Now;
+        }
+
+        public bool UpgradeDevice(int DeviceID)
+        {
+            return false;
+        }
+    }
+}
